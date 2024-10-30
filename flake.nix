b5e77a9a8f7a7372f31bd0b48bc98fdaea0b5548
Jad Zahreddine <107253062+jad-zd@users.noreply.github.com>
2024-10-30 16:16:11 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in
+    {
+      packages.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          coq
+          coqPackages.coqide
+        ];
+
+        shellHook = "exec $SHELL";
+      };
+    };
+}
