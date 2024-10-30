5db1e2fc5f2976a997404c571f6ee9e0742c2380
Jad Zahreddine <107253062+jad-zd@users.noreply.github.com>
2024-10-30 16:19:37 +0100

    4 - Équivalence entre les deux sémantiques

--- Equiv.v
+++ Equiv.v
@@ -11,6 +11,9 @@ Open Scope string_scope.
 Require Import While Tactics.
 Require Import WhileSmallStep WhileBigStep.
 
+Print star.
+Print step.
+
 Lemma star_seq:
   forall s1 env1 env2,
   star s1 env1 Skip env2
@@ -20,13 +23,32 @@ Lemma star_seq:
 Proof.
   intros s1 env1 env2 Hstar.
   dependent induction Hstar; simpl; intros.
-Admitted.
+  - eapply star_step. apply step_seq_skip. tauto.
+  - apply IHHstar in H0.
+    eapply star_step.
+    apply step_seq.
+    apply H.
+    apply H0.
+    tauto.
+Qed.
 
 
 Theorem bigstep_star:
   forall i env env', bigstep env i env' -> star i env Skip env'.
 Proof.
-Admitted.
+  intros i env env' H.
+  induction H.
+  - apply star_refl.
+  - eapply star_step. apply step_assign. apply star_refl.
+  - eapply star_seq. apply IHbigstep1. apply IHbigstep2.
+  - eapply star_step. apply step_if_true. apply H. apply IHbigstep.
+  - eapply star_step. apply step_if_false. apply H. apply IHbigstep.
+  - eapply star_step. apply step_while_false. apply H. apply star_refl.
+  - eapply star_step. apply step_while_true. apply H. eapply star_seq.
+   * apply IHbigstep1.
+   * apply IHbigstep2.
+Qed.   
+
 
 Lemma step_bigstep:
   forall i env i' env',
@@ -35,11 +57,23 @@ Lemma step_bigstep:
       bigstep env' i' env'' ->
       bigstep env i env''.
 Proof.
-Admitted.
+  intros i env i' env' H.
+  induction H.
+  - intros. inversion H. apply bigstep_assign.
+  - intros. apply bigstep_if_true. apply H. apply H0.
+  - intros. apply bigstep_if_false. apply H. apply H0.
+  - intros. inversion H0. eapply bigstep_while_true. apply H. apply H4. apply H6.
+  - intros. inversion H0. eapply bigstep_while_false. rewrite H3 in H. apply H.
+  - intros. inversion H0. eapply bigstep_seq. apply IHstep. apply H4. apply H6.
+  - intros. eapply bigstep_seq. eapply bigstep_skip. apply H.
+Qed.
+
 
 Theorem star_bigstep:
   forall i env env', star i env Skip env' -> bigstep env i env'.
 Proof.
-Admitted.
-
-
+  intros i env env' H.
+  dependent induction H.
+  - eapply bigstep_skip.
+  - eapply step_bigstep. eapply H. eapply IHstar. tauto.
+Qed.     
